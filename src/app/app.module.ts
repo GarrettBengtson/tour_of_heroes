5b9306bbeb5edc16b6052f2bccc3627aab3a14e9
Garrett Bengtson <garrett.bengtson@ndsu.edu>
2024-04-28 19:32:48 -0500

    Added a HEROES list to display heroes. Added a details view for specific heroes. Added two way binding for hero name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { FormsModule } from '@angular/forms'; // NgModel lives here
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
